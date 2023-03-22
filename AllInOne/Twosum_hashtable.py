0722e59fd0817fa28c74267008287c575dee1ea1
LAPTOP-JTHOJ1JL\suhyeon <shkang2511@gmail.com>
2023-03-22 23:56:34 +0900

    [Fix] fix test cases of dictionary example

--- AllInOne/Twosum_hashtable.py
+++ AllInOne/Twosum_hashtable.py
@@ -9,6 +9,6 @@ def twoSum(nums, target) :
 
     return False
 
-# print(twoSum(nums=[2,7,11,15], target=9))        
-# print(twoSum(nums=[3,3], target=6))
+print(twoSum(nums=[2,7,11,15], target=9))        
+print(twoSum(nums=[3,3], target=6))
 print(twoSum(nums=[3,2,4], target=6))
