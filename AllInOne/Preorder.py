ad64fcb40bc7a2bc79e685c2b837aa9b602aeaaf
LAPTOP-JTHOJ1JL\suhyeon <shkang2511@gmail.com>
2023-04-04 20:29:30 +0900

    [Add] implementation of preorder traversal

--- AllInOne/Preorder.py
+++ AllInOne/Preorder.py
@@ -1,1 +1,19 @@
 from collections import deque
+
+def Preorder(root) :
+    
+    visited = []
+
+    q = deque()
+    q.append(root)
+
+    while q :
+        current_node = q.popleft()
+        visited.append(current_node)
+
+        if current_node.left :
+            Preorder(current_node.left)
+        if current_node.right :
+            Preorder(current_node.right)
+
+    return visited        
