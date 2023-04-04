cfb827b106ee606adcdf65429e4d38c44a8b1751
LAPTOP-JTHOJ1JL\suhyeon <shkang2511@gmail.com>
2023-04-04 20:35:35 +0900

    [Fix] fix code in a way that does not using deque

--- AllInOne/Preorder.py
+++ AllInOne/Preorder.py
@@ -4,16 +4,13 @@ def Preorder(root) :
     
     visited = []
 
-    q = deque()
-    q.append(root)
-
-    while q :
-        current_node = q.popleft()
-        visited.append(current_node)
-
-        if current_node.left :
-            Preorder(current_node.left)
-        if current_node.right :
-            Preorder(current_node.right)
+    
+    current_node = root
+    if current_node not in visited :
+        visited.append(current_node.value)
+    if current_node.left :
+        Preorder(current_node.left)
+    if current_node.right :
+        Preorder(current_node.right)
 
     return visited        
